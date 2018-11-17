1cc216dfff3655f14a1a07998aa916934ae706ef
Michal Stanke <michal@stanke.cz>
2018-11-17 09:41:20 +0100

    Add .ico suffix to downloaded files

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 cd $1;
 while read line; do
     url="$(cut -d ',' -f 1 <<< "$line")"
-    filename="$(cut -d ',' -f 2 <<< "$line")"
+    filename="$(cut -d ',' -f 2 <<< "$line").ico"
     curl $url --output $filename
     if file --mime-type "$filename" | grep -q gzip$; then
         echo "${filename} is gzipped"
@@ -15,4 +15,3 @@ done < "./searchplugins.csv";
 md5sum --check "./MD5SUMS";
 
 exit $?;
-
