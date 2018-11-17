e37e270cbd90a5156f5f31f75bafef868462d73c
Michal Stanke <michal@stanke.cz>
2018-11-17 09:39:29 +0100

    Fix for gzipped icons

--- check.sh
+++ check.sh
@@ -5,6 +5,11 @@ while read line; do
     url="$(cut -d ',' -f 1 <<< "$line")"
     filename="$(cut -d ',' -f 2 <<< "$line")"
     curl $url --output $filename
+    if file --mime-type "$filename" | grep -q gzip$; then
+        echo "${filename} is gzipped"
+	mv "$filename" "${filename}.gz"
+	gunzip "$filename"
+    fi
 done < "./searchplugins.csv";
 
 md5sum --check "./MD5SUMS";
